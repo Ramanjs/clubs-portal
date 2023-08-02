aa0d7b23acffe89789a9501132c225e373d1d587
Ramanjs <raman.s2662@gmail.com>
2023-08-02 16:51:13 +0530

    upcoming events

--- controllers/club.ts
+++ controllers/club.ts
@@ -1,5 +1,6 @@
 import { type Request, type Response } from 'express'
 import Club from '../models/club'
+import Event from '../models/event'
 
 const getAllClubs = async (req: Request, res: Response): Promise<Response> => {
   const clubs = await Club.find()
@@ -10,10 +11,13 @@ const getAboutInfo = async (req: Request, res: Response): Promise<Response> => {
   const { id } = req.params
   const club = await Club.findOne({ handle: id }).populate({ path: 'coordinator', select: 'name email handle' }).lean()
 
+  const events = await Event.find({ club: club?._id, status: 'APPROVED' })
+
   const response = {
     ...club,
     // @ts-expect-error idk
-    isCoordinator: club.coordinator.handle === req.user.handle
+    isCoordinator: club.coordinator.handle === req.user.handle,
+    events
   }
 
   return res.status(200).json(response)
