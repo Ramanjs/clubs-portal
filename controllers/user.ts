27dc7d39099cb4d416643331acc5d05966963698
Ramanjs <raman.s2662@gmail.com>
2023-08-02 11:47:45 +0530

    send requests

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,10 +1,14 @@
 import { type Request, type Response } from 'express'
-
+import Event from '../models/event'
 import User from '../models/user'
 
 const getUserInfo = async (req: Request, res: Response): Promise<Response> => {
   const { id } = req.params
-  const user = await User.findOne({ handle: id })
+  const user = await User.findOne({ handle: id }).lean()
+
+  const events = await Event.find({ coordinator: user?._id }).lean()
+  // @ts-expect-error idk
+  user.requests = events
 
   return res.status(200).json(user)
 }
