3e8cbebd1838ceb18714dd75b738860d01c6f89c
Ramanjs <raman.s2662@gmail.com>
2023-08-10 18:37:22 +0530

    add route to create club

--- routes/club.ts
+++ routes/club.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import catchAsync from '../utils/catchAsync'
-import { getAllClubs, getAboutInfo } from '../controllers/club'
+import { getAllClubs, getAboutInfo, createClub } from '../controllers/club'
 import requiresAuth from '../middleware/auth'
 
 const router = express.Router()
@@ -8,6 +8,12 @@ const router = express.Router()
 router.get(
   '/',
   catchAsync(getAllClubs)
+)
+
+router.post(
+  '/',
+  requiresAuth,
+  catchAsync(createClub)
 )
 
 router.get(
